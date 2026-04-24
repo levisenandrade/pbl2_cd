118b5738764e8521de2bfdd88c233e6f4e0b8389
Levi <levisenandrade@gmail.com>
2026-04-24 15:57:34 -0300

    Add files via upload

---
+++ pbl.v
@@ -0,0 +1,49 @@
+module pbl (
+    input clk,
+    input reset,
+    output [4:0] q,  // A declaração já acontece aqui
+	 output sinal_trava
+);
+
+wire [3:0] carry;
+wire trava;
+wire enable;
+
+// Lógica de travamento (Detecta o valor 20: 10100)
+wire n_q3, n_q1, n_q0;
+not (n_q3, q[3]);
+not (n_q1, q[1]);
+not (n_q0, q[0]);
+and (trava, q[4], n_q3, q[2], n_q1, n_q0);
+
+// Inverte a trava para criar um sinal de "permite contagem"
+not (enable, trava);
+
+and (sinal_trava, q[4], n_q3, q[2], n_q1, n_q0);
+
+// --- BIT 0 ---
+// No JK, J=K=1 faz o bit inverter (Toggle). 
+// Ele inverte sempre que o enable for 1.
+flipflopJK ff0 (.clk(clk), .reset(reset), .j(enable), .k(enable), .q(q[0]));
+
+// --- BIT 1 ---
+// Inverte se (enable AND q[0])
+and (carry[0], enable, q[0]);
+flipflopJK ff1 (.clk(clk), .reset(reset), .j(carry[0]), .k(carry[0]), .q(q[1]));
+
+// --- BIT 2 ---
+// Inverte se (carry[0] AND q[1])
+and (carry[1], carry[0], q[1]);
+flipflopJK ff2 (.clk(clk), .reset(reset), .j(carry[1]), .k(carry[1]), .q(q[2]));
+
+// --- BIT 3 ---
+// Inverte se (carry[1] AND q[2])
+and (carry[2], carry[1], q[2]);
+flipflopJK ff3 (.clk(clk), .reset(reset), .j(carry[2]), .k(carry[2]), .q(q[3]));
+
+// --- BIT 4 ---
+// Inverte se (carry[2] AND q[3])
+and (carry[3], carry[2], q[3]);
+flipflopJK ff4 (.clk(clk), .reset(reset), .j(carry[3]), .k(carry[3]), .q(q[4]));
+
+endmodule
